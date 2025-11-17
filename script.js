f9eadd8db93123ad27ef075af263e9f14d0e7bc9
JeannePeuron <jeanne.peuron@gmail.com>
2025-11-17 10:54:46 +0100

    fonctions readForm et addQuote

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
+const form = document.getElementById("quotationsAdd");
 const text = document.getElementById("textQuotation");
-const author = document.getElementById("author");
-const search = document.getElementById("search");
+const textAuthor = document.getElementById("textAuthor");
+const quoteList = document.getElementById("quote-list");
 
-function recoverParam() {
-  search.addEventListener(`click`, () => {
-    // console.log(text.value);
-    // console.log(author.value);
-  });
+form.addEventListener("submit", readForm);
+
+function readForm(event) {
+  event.preventDefault();
+
+  const quote = text.value;
+  const author = textAuthor.value;
+
+  addQuote(quote, author);
 }
 
-recoverParam();
+function addQuote(quote, author) {
+  const div = document.createElement("div");
+  div.classList.add("quote");
+
+  div.innerHTML = `
+    <p class="text">Citation : ${quote}</p>
+    <p class="author">Auteur.ice : ${author}</p>
+  `;
+
+  quoteList.appendChild(div);
+}
+
+// Nous allons maintenant afficher les citations saisies directement sur la page.
+
+// - Créez une fonction addQuote(quote, author) prenant deux paramètres (quote et author, du coup).
+// - Dans cette fonction :
+//     - Créez un élément <p> pour la citation et attribuez-lui la classe "text".
+//     - Créez un élément <p> pour l’auteur/autrice et attribuez-lui la classe "author".
+//     - Insérez les valeurs des paramètres quote et author dans ces éléments.
+//     - Créez une <div> avec la classe "quote", puis ajoutez-y les deux paragraphes.
+//     - Récupérez l’élément ayant l’id "quote-list" et ajoutez-y la nouvelle citation avec
+// appendChild().
+// - Modifiez le gestionnaire d’événements du formulaire pour appeler addQuote() au lieu
+// de simplement afficher les valeurs dans la console.
+
+// Besoin d'un coup de main ? Vous pouvez créer un élément en JS avec la fonction
+// document.createElement('div')
